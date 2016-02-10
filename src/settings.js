a1c20275d3e26f91afa009271722c4a2cbd57060
m0ose <cody@simtable.com>
2016-02-10 14:19:22 -0700

    bug fix. WIFI router was crashing. dang

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,6 @@
 export var settings={
-  firebaseURL : "https://torrid-torch-716.firebaseio.com/",
-  CHUNK_SIZE : Math.pow(2,13) // size in bytes of the chunks. 2^16 is just under the limit in chrome.
+  firebaseURL : "https://torrid-torch-716.firebaseio.com/"
+  // Was having a bug where the WIFI router would crash if the chunk size was bigger than 2^10
+  CHUNK_SIZE : Math.pow(2,10) // size in bytes of the chunks. 2^16 is just under the limit in chrome.
+                            // Was
 }
