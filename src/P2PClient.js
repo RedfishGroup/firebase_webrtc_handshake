c92f717e9f2047ec610b1811d404df1e2dcf1bca
m0ose <cody@simtable.com>
2016-02-16 12:07:08 -0700

    renamed files/classes

---
+++ src/P2PClient.js
@@ -0,0 +1,109 @@
+import {PeerBinary} from "./PeerBinary.js"
+import "bower_components/firebase/firebase.js"
+import {settings} from "./settings.js"
+import {Evented} from "./Evented.js"
+
+export class P2PClient extends Evented{
+  constructor(options={}) {
+    super()
+    _.extend(this,settings)
+    _.extend(this,options)
+    this.fbref = new Firebase(this.firebaseURL).child('peers')
+    this.connection = null
+    this.channelRef = null
+    this.debug = false
+  }
+
+  getPeerList(callback) {
+    this.fbref.once('value',(ev)=>{
+      var val = ev.val()
+      this.peerList = val
+      callback(null, val)
+    })
+  }
+
+  connectToPeerID(id, callback=()=>{}) {
+    this.getPeerList(()=>{
+      var peer = this.peerList[id]
+      if(!peer) {
+        console.error('peer not defined. id:', id)
+        callback("peer not defined")
+      } else {
+        this.serverRef = this.fbref.child(id)
+        var p = new PeerBinary({ initiator: true, trickle: true , iceServers: this.ICE_SERVERS })
+        this.connection = p
+        this._registerEvents()
+        p.on('signal', (data)=>{
+          if(data.type == "offer") {
+            this._createChannel(data)
+          } else if(data.candidate){
+            if(this.debug) console.log('client recieved candidate from webrtc', data)
+            this.outRef.push(data)
+          }else {
+            console.warn('Client recieved unexpected signal through WebRTC:', data)
+          }
+        })
+        callback(null, this.connection)
+      }
+    })
+  }
+
+  disconnect(callback) {
+    callback = callback || function(){console.log('client disconnected from server', arguments)}
+    if(this.serverRef) { this.serverRef.off() }
+    if(this.outRef) { this.outRef.off() }
+    if(this.inRef) {this.inRef.off()}
+    if(this.connection) {
+      this.connection.destroy(callback)
+    } else{
+      callback()
+    }
+    // QUESTION: should I also disconnect from the listeners to the events emitted by this class?
+    //     it would be this.off()
+  }
+
+  _createChannel(offer) {
+    //this.channelRef = this.serverRef.child('channels').push({offer:offer})
+    this.channelRef = this.serverRef.child('channels').push({
+      fromClient:[offer]
+    })
+    this.outRef = this.channelRef.child('fromClient')
+    this.inRef = this.channelRef.child('fromServer')
+    this.inRef.on('child_added',(ev)=>{
+      if(this.debug) console.log('channel message, client', ev.val())
+      var val = ev.val()
+      if(val.type == 'answer') {
+        setTimeout(()=>{this.connection.signal(val)}, 1)// a slight delay helps establish connection, I think.
+      } else if(val.candidate) {
+        if(this.debug) console.log('client recieved candidate from firebase')
+        setTimeout(()=>{this.connection.signal(val)}, 1)
+      } else {
+        console.warn('Client recieved unexpected signal through Firebase', val)
+      }
+    })
+  }
+
+
+  _registerEvents() {
+    // fire events
+    this.connection.on('error', (err)=>{
+      console.error('client: error', err)
+      this.fire('error',{peer:this.connection, err:err})
+    })
+    this.connection.on('connect', ()=>{
+      if(this.debug) console.log('client: client connected')
+      this.fire('connect',{peer:this.connection})
+    })
+    this.connection.on('data', (data)=>{
+      if(this.debug) console.log('server: server recieved some data: ',data)
+      this.fire('data',{peer:this.connection, data:data})
+    })
+    this.connection.on('close', (data)=>{
+      if(this.debug) console.log('connection closed', this.connection)
+      this.fire('close',{peer:this.connection})
+    })
+    this.connection.on('dataBig', (data)=>{
+      this.fire('dataBig',{peer:this.connection, data:data})
+    })
+  }
+}
---
+++ src/P2PServer.js
@@ -0,0 +1,153 @@
+import {PeerBinary} from "./PeerBinary.js"
+import "bower_components/firebase/firebase.js"
+import {settings} from "./settings.js"
+import {Evented} from "./Evented.js"
+
+class Channel{
+
+  constructor(fbref, peer) {
+    this.outRef = fbref.child("fromServer") //firebase
+    this.inRef = fbref.child("fromClient")
+    this.peer = peer // simple-peer
+  }
+
+  destroy() {
+    this.outRef.off()
+    this.inRef.off()
+    this.peer.destroy()
+  }
+}
+
+export class P2PServer extends Evented{
+
+  constructor(options={}) {
+    super() //no idea what this does
+    this.MAX_CONNECTIONS = 20
+    this.debug = false
+    this.id = "server"+Math.floor(Math.random()*100000)
+    this.firebaseURL = settings.firebaseURL
+    _.extendOwn(this, options)
+    if(this.debug) console.log(this.id)
+    this.init()
+  }
+
+  init(){
+    var fbref = new Firebase(this.firebaseURL).child('peers')
+    this.userRef = fbref.child(this.id)
+    this.updateRef = this.userRef.child('lastUpdate')
+    this.userRef.onDisconnect().remove()
+    this.updateRef.set(new Date().getTime())
+    this.channelRef = this.userRef.child('channels')
+    this.channelRef.set([])
+    this.connections = []
+    this._intervalID = setInterval(()=>{
+      this.updateRef.set(new Date().getTime())
+    }, 4000)
+    this.listenToChannels()
+  }
+
+  sendToAll( data) {
+    for(var conx of this.connections) {
+      if(this.debug) console.log(conx)
+      conx.peer.send(data)
+    }
+  }
+
+  listenToChannels() {
+    // when a new channel is added, listen to it.
+    this.channelRef.on('child_added', (ev, prevKey)=>{
+      if(this.connections.length > this.MAX_CONNECTIONS) {
+        console.error('Too many connections. TODO:close/remove old stail connections')
+        return
+      }
+      var val = ev.val()
+      for(var i in val.fromClient) {
+        var sig = val.fromClient[i]
+        if(sig.type == 'offer') {
+          var channel = new Channel(this.channelRef.child(ev.key()), this._makePeer())
+          this.connections.push(channel)
+          // on message through webRTC (simple-peer)
+          channel.peer.on('signal', (data)=>{
+            if(data.type == "answer") {
+              channel.outRef.push(data)
+            } else if(data.candidate) {
+              channel.outRef.push(data)
+            }else {
+              console.warn('unexpected message from WebRTC', data)
+            }
+          })
+          // on message through firebase
+          channel.inRef.on('child_added', (ev2)=>{
+            var val2 = ev2.val()
+            if(val2.candidate) {
+              if(this.debug) console.log('server got candidate from firebase', val2)
+              channel.peer.signal(val2)
+            } else if(val2.type == 'offer') {
+              channel.peer.signal(val2)
+            } else if(val2.type == 'answer') {
+              //ignore this. It was probably from me.
+            } else {
+              console.warn('unexpected message from Firebase', val2)
+            }
+          })
+        }
+      }
+    })
+  }
+
+  _makePeer() {
+    if(this.debug) console.log('_makePeer called')
+    var p = new PeerBinary({ initiator: false, trickle: true, iceServers: settings.ICE_SERVERS})
+    // fire events
+    p.on('error', (err)=>{
+      console.error('server: error', err)
+      this.fire('error',{peer:p, err:err})
+    })
+    p.on('connect', ()=>{
+      if(this.debug) console.log('server: client connected')
+      this.fire('connect',{peer:p})
+    })
+    p.on('data', (data)=>{
+      if(this.debug) console.log('server: server recieved some data: ',data)
+      this.fire('data',{peer:p, data:data})
+    })
+    p.on('close', ()=>{
+      if(this.debug) console.log('server: connection closed', p)
+      this._removeConnection(p)
+      this.fire('close',{peer: p})
+    })
+    p.on('dataBig', (data)=>{
+      this.fire('dataBig',{peer:p, data:data})
+    })
+    //TODO make it so server can register events that will get called on each individual connection
+    return p
+  }
+
+  destroy() {
+    this.channelRef.off()
+    this.updateRef.off()
+    this.userRef.off()
+    for(var x of this.connections) {
+      x.destroy()
+    }
+    this.connections = []
+    clearInterval(this.intervalID)
+  }
+
+  _removeConnection(peer) {
+    var index=-1
+    for(var i=0; i<this.connections.length; i++) {
+      var conn = this.connections[i]
+      if(conn.peer == peer) {
+        console.log('found my connection', i, conn)
+        index = i
+      }
+    }
+    if(index>=0) {
+      var conn = this.connections[index]
+      conn.destroy()
+      this.connections.splice(index,1)
+      console.log(this.connections)
+    }
+  }
+}
