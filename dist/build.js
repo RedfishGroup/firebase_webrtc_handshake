69796ea3ae1433372a314068bfe808fe371ee01c
Joshua Thorp <joshua@stigmergic.net>
2019-09-27 15:41:55 -0600

    build, version bump,  testing large webrtc packet size 16Kb

--- dist/build.js
+++ dist/build.js
@@ -45,25 +45,25 @@ class Evented {
 }
 
 var settings = {
-  // Get a reference to the database service
+    // Get a reference to the database service
 
-  // Was having a bug where the WIFI router would crash if the chunk size was bigger than 2^10
-  CHUNK_SIZE: Math.pow(2, 10), // size in bytes of the chunks. 2^16 is just under the limit in chrome.
-  ICE_SERVERS: [
-    {
-      url: 'stun:23.21.150.121',
-      urls: 'stun:23.21.150.121',
-    },
-    {
-      url: 'turn:global.turn.twilio.com:3478?transport=udp',
-      username:
-        '508d1e639868dc17f5da97a75b1d3b43bf2fc6d11e4e863678501db568b5665c',
-      credential: 'W5GTdhQQ6DqOD7k6bS8+xZVNQXm+fgLXSEQpN8bTe70=',
-      urls: 'turn:global.turn.twilio.com:3478?transport=udp',
-    },
-  ],
-  POLLING_FREQUENCY: 15000,
-  debug: false,
+    // Was having a bug where the WIFI router would crash if the chunk size was bigger than 2^10
+    CHUNK_SIZE: Math.pow(2, 14), // size in bytes of the chunks. 2^14 is just under the limit in chrome.
+    ICE_SERVERS: [
+        {
+            url: 'stun:23.21.150.121',
+            urls: 'stun:23.21.150.121',
+        },
+        {
+            url: 'turn:global.turn.twilio.com:3478?transport=udp',
+            username:
+                '508d1e639868dc17f5da97a75b1d3b43bf2fc6d11e4e863678501db568b5665c',
+            credential: 'W5GTdhQQ6DqOD7k6bS8+xZVNQXm+fgLXSEQpN8bTe70=',
+            urls: 'turn:global.turn.twilio.com:3478?transport=udp',
+        },
+    ],
+    POLLING_FREQUENCY: 15000,
+    debug: false,
 };
 
 var msgPack = msgpacklite__default;
