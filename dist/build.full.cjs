ac0d42ed896b86aa9a21f8d32f3a6d99c75316f3
Joshua Thorp <joshua@stigmergic.net>
2021-12-10 14:10:42 -0700

    more debug

--- dist/build.full.cjs
+++ dist/build.full.cjs
@@ -16073,9 +16073,11 @@ function P2PServerFactory(options) {
             if (this.initialPeerInfo) {
                 console.log('UserRef: ' + this.userRef, this.initialPeerInfo);
                 console.log('Got Here 1');
-                update(this.userRef, this.initialPeerInfo).catch((e) => {
-                    console.log('FWH Got Error: ', e);
-                });
+                update(this.userRef, this.initialPeerInfo)
+                    .then((v) => console.log('success: ', v.val()))
+                    .catch((e) => {
+                        console.log('problem: ', e);
+                    });
             }
             console.log('Got Here 2');
 
--- dist/build.js
+++ dist/build.js
@@ -15316,9 +15316,11 @@ function P2PServerFactory(options) {
             if (this.initialPeerInfo) {
                 console.log('UserRef: ' + this.userRef, this.initialPeerInfo);
                 console.log('Got Here 1');
-                update(this.userRef, this.initialPeerInfo).catch((e) => {
-                    console.log('FWH Got Error: ', e);
-                });
+                update(this.userRef, this.initialPeerInfo)
+                    .then((v) => console.log('success: ', v.val()))
+                    .catch((e) => {
+                        console.log('problem: ', e);
+                    });
             }
             console.log('Got Here 2');
 
