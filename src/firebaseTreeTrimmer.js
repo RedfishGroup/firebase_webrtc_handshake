e0971e0651178b56ee29a1c5f0730f212b19aa12
Joshua Thorp <joshua@stigmergic.net>
2023-05-05 14:23:08 -0600

    lost this?

--- src/firebaseTreeTrimmer.js
+++ src/firebaseTreeTrimmer.js
@@ -15,9 +15,9 @@ export class firebaseTreeTrimmer {
             )
         Object.assign(this, options)
 
+        this.firebase = options.firebase
         this.monitorReference = this.monitor.bind(this)
         this.monitor()
-        this.firebase = options.firebase
     }
 
     monitor() {
@@ -77,7 +77,7 @@ export class firebaseTreeTrimmer {
         this.firebase.onValue(
             this.peersRef,
             (snap) => {
-                snap.forEach(function (child) {
+                snap.forEach((child) => {
                     // if the peer is not in the treeTrimming list,
                     // remove it from peersRef
                     if (treeTrimmers[child.key] === undefined) {
